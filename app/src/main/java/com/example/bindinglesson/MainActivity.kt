a3aa1a9a2735302208d681efd63ee8ca0636943d
DavidMarkovich1218 <87656402+DavidMarkovich1218@users.noreply.github.com>
2022-03-25 11:57:05 +0300

    RandomWordGenerator v0.0.2 ViewBinding instead FindViewById, Added "Word Lenght" and "Begins With" parameters

--- app/src/main/java/com/example/bindinglesson/MainActivity.kt
+++ app/src/main/java/com/example/bindinglesson/MainActivity.kt
@@ -7,7 +7,7 @@ import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
-//import com.example.bindinglesson.databinding.ActivityMainBinding
+import com.example.bindinglesson.databinding.ActivityMainBinding
 import com.mashape.unirest.http.Unirest
 import org.jetbrains.anko.activityUiThread
 import org.jetbrains.anko.doAsync
@@ -17,52 +17,47 @@ import org.json.JSONObject
 import org.json.JSONStringer
 import kotlin.random.Random
 
-//lateinit var bindingClass: ActivityMainBinding
+lateinit var bindingClass: ActivityMainBinding
 class MainActivity : AppCompatActivity() {
+    private fun apiRequest() {
+        var wordLenght = bindingClass.editWordLenght.text
+        var startWith = bindingClass.editStartWith.text
+        doAsync {
+            var responce = Unirest.get("https://random-words5.p.rapidapi.com/getMultipleRandom?count=1&wordLength="+wordLenght+"&beginsWith="+startWith)
+                .header("X-RapidAPI-Host", "random-words5.p.rapidapi.com")
+                .header("X-RapidAPI-Key", "dfd13661dbmsh0610c2e8200745dp19acd2jsn8894c165bbc9")
+                .asString();
+            var all = responce.body
+
+            activityUiThread {
+                bindingClass.changeText.text = all.toString()
+            }
+        }
+    }
+
+    private fun apiRequestRandom(){
+        doAsync { var responce = Unirest.get("https://random-words5.p.rapidapi.com/getRandom")
+            .header("X-RapidAPI-Host", "random-words5.p.rapidapi.com")
+            .header("X-RapidAPI-Key", "dfd13661dbmsh0610c2e8200745dp19acd2jsn8894c165bbc9")
+            .asString();
+            var all = responce.body
+
+            activityUiThread {
+                bindingClass.changeText.text = all.toString()
+            }}
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        bindingClass = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(R.layout.activity_main)
-        doAsync {
-            var responce = Unirest.get("https://random-words5.p.rapidapi.com/getRandom")
-                .header("X-RapidAPI-Host", "random-words5.p.rapidapi.com")
-                .header("X-RapidAPI-Key", "dfd13661dbmsh0610c2e8200745dp19acd2jsn8894c165bbc9")
-                .asString();
-            var all = responce.body
-
-            activityUiThread {
-                var text = findViewById<TextView>(R.id.changeText)
-                text.text = all.toString()
-            }
-            //fun onCLickChange(view: View) {
-//          var list = listOf("few words","edwdw","sdasda","asdadasdsa", "rfweewf")
-//          text = bindingClass.changeText
-//          var randomIndex = Random.nextInt(list.size)}
-
-        }
+        bindingClass = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(bindingClass.root)
+        apiRequestRandom()
 
     }
 
+
     fun onClcChange(view: View){
-        doAsync {
-            var responce = Unirest.get("https://random-words5.p.rapidapi.com/getRandom")
-                .header("X-RapidAPI-Host", "random-words5.p.rapidapi.com")
-                .header("X-RapidAPI-Key", "dfd13661dbmsh0610c2e8200745dp19acd2jsn8894c165bbc9")
-                .asString();
-            var all = responce.body
-
-            activityUiThread {
-                var text = findViewById<TextView>(R.id.changeText)
-                text.text = all.toString()
-            }
-            //fun onCLickChange(view: View) {
-//          var list = listOf("few words","edwdw","sdasda","asdadasdsa", "rfweewf")
-//          text = bindingClass.changeText
-//          var randomIndex = Random.nextInt(list.size)}
-
-        }
-
+        apiRequest()
     }
 
 }
